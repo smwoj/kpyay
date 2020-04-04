338d6b6be814ff8a893dffb305495629e4ac8fa5
Wojciech Smołka <sm.wojciech@gmail.com>
2020-04-04 16:37:13 +0200

    removed elm boilerplate

--- server/src/main/scala/io/github/smwoj/kpayyserver/Main.scala
+++
@@ -1,9 +0,0 @@
-package io.github.smwoj.kpayyserver
-
-import cats.effect.{ExitCode, IO, IOApp}
-import cats.implicits._
-
-object Main extends IOApp {
-  def run(args: List[String]) =
-    KpayyserverServer.stream[IO].compile.drain.as(ExitCode.Success)
-}
