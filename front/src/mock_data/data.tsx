29f9258d33f535285a9f5e5ca99a2d1e6dbe3c2b
Wojciech Smołka <sm.wojciech@gmail.com>
2020-04-08 14:25:39 +0200

    mock data parsing

--- front/src/mock_data/data.tsx
+++ front/src/mock_data/data.tsx
@@ -1,9 +1,25 @@
 const fs = require('fs');
 import {metricPoint} from '../store/models'
 
-export const parsePoints: metricPoint[] = (json: string) => {
-    JSON.parse(json).map
+export interface rawPoint {
+    value: number,
+    version?: string,
+    params: { [param: string]: string }
+    timestamp?: string,
+    posted_ts: string,
+}
+
+export const parsePoints = (json: string): metricPoint[] => {
+    const objs: rawPoint[] = JSON.parse(json);
+    return objs.map((point) => {
+        const timestamp: Date | undefined = point.timestamp ? new Date(point.timestamp) : undefined;
+        return {...point, posted_ts: new Date(point.posted_ts), timestamp};
+    });
 };
 
-export const DOGS_VS_MUFFINS_FSCORES: metricPoint[] = fs.readFileSync('./dogs_muffins_classifiers.json');
-export const SLOTHS_VS_PASTRY_FSCORES: metricPoint[] = fs.readFileSync();
+export const DOGS_VS_MUFFINS_FSCORES: metricPoint[] = parsePoints(
+    fs.readFileSync('./dogs_muffins_classifiers.json')
+);
+export const SLOTHS_VS_PASTRY_FSCORES: metricPoint[] = parsePoints(
+    fs.readFileSync('./sloths_pastry_classifiers.json')
+);
