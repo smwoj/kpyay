2cab63ffd5f456fa913fe6dffbccec9267871e3a
Wojciech Smołka <sm.wojciech@gmail.com>
2020-04-17 22:44:09 +0200

    [server] cors added

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,20 @@
+use actix_cors::Cors;
 use actix_web::{get, web, App, HttpServer, Responder};
-
 
 #[get("/{slug}")]
 async fn index(info: web::Path<String>) -> impl Responder {
-    format!("Hello from '{}'! ", info)
+    serde_json::json!({ "echoed": info.into_inner() }).to_string()
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            // https://docs.rs/actix-cors/0.2.0/actix_cors/index.html
+            .wrap(Cors::new().allowed_methods(vec!["GET"]).finish())
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
